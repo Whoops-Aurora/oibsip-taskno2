73c7d527e5fe86c7b35d6b9c43b115e73160f060
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-02 13:16:13 +0530

    "Home page with avatar is done."

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,3 +1,8 @@
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles'
+import React,{ useCallback } from 'react';
+
+
 const ParticlesContainer = () => {
   return <div>Particles Container</div>;
 };
