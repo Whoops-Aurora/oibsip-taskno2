e295f04eefc2d946969e37cdb846e4b468cb4d30
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-04 20:37:27 +0530

    "About page complete now starting with services"

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ import Socials from '../components/Socials';
 const Header = () => {
   return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
     <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row jusify-between items-center gap-y-6 py-8" >
+      <div className="flex flex-1 flex-col lg:flex-row jusify-between items-center gap-y-6 py-8" >
         {/* logo */}
         <Link href={'/'}>
           <Image 
