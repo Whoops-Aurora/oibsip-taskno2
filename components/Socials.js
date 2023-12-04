401641accb71f78a1981e801bb05e4f63d4fcf50
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-04 22:42:21 +0530

    "updated social.js with my links"

--- components/Socials.js
+++ components/Socials.js
@@ -13,22 +13,19 @@ import { RiYoutubeLine,
 const Socials = () => {
   return (
   <div className='flex items-center gap-x-5 text-lg'>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
-   <RiYoutubeLine/>
-   </Link>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
+   <Link href={'https://instagram.com/adwait.api?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr'} className='hover:text-accent transition-all duration-300'>
    <RiInstagramLine/>
    </Link>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
+   <Link href={'https://github.com/Whoops-Aurora'} className='hover:text-accent transition-all duration-300'>
    <RiGithubLine/>
    </Link>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
+   <Link href={'https://twitter.com/_ForstY_'} className='hover:text-accent transition-all duration-300'>
    <RiTwitterXLine/>
    </Link>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
+   <Link href={'https://www.linkedin.com/in/adwait-s-a1362a1a0/'} className='hover:text-accent transition-all duration-300'>
    <RiLinkedinLine/>
    </Link>
-   <Link href={''} className='hover:text-accent transition-all duration-300'>
+   <Link href={'https://t.me/TheNameNotTakenAdwait'} className='hover:text-accent transition-all duration-300'>
    <RiTelegramLine/>
    </Link>
   </div>
