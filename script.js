bb5156f164ff0b38113757a3c1161201757a1866
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-12 23:51:18 +0530

    "Addded changes and images"

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute('href'));
-
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    const trail = document.createElement('div');
+    trail.className = 'cursor-trail';
+    document.body.appendChild(trail);
+  
+    document.addEventListener('mousemove', function(e) {
+      const trailSize = 20; // Change the size of the trail
+      const x = e.pageX;
+      const y = e.pageY;
+      const trailElement = document.querySelector('.cursor-trail');
+      trailElement.style.left = x - trailSize / 2 + 'px';
+      trailElement.style.top = y - trailSize / 2 + 'px';
     });
-});
-
-// Form submission handling
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = this.querySelector('input[type="email"]').value;
-    // Perform actions with the email (like sending to a server, etc.)
-    alert(`Thank you for subscribing with ${email}!`);
-    this.reset();
-});
-
-// Fetch a random image from Lorem Picsum
-fetch('https://picsum.photos/1920/1080')
-.then(response => {
-  const imageUrl = response.url;
-  // Set the body's background image to the fetched random image
-  document.body.style.backgroundImage = `url(${imageUrl})`;
-})
-.catch(error => {
-  console.error('Error fetching random image:', error);
-});
+  });  
